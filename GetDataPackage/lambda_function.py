60e84ae63b3e38d49639c04fa4befeeb014e7ec5
unknown <>
2022-07-08 12:34:11 -0700

    change SQS message format

--- GetDataPackage/lambda_function.py
+++ GetDataPackage/lambda_function.py
@@ -3,6 +3,7 @@ lambda function gets current pi cases from database and send them to sqs.
 """
 import os
 import logging
+import json
 from get_data import create_month_end
 import boto3
 
@@ -27,10 +28,13 @@ def lambda_handler(event, context):
     client = boto3.client('sqs',region_name='us-west-2',
     endpoint_url='https://sqs.us-west-2.amazonaws.com')
     queueInfo = client.get_queue_url(QueueName='MonthEnd')
-    for clinicInfo in result.items():
+    for clinic in result:
+        message=result[clinic]
+        message['clinic']=clinic
+        messageJson=json.dumps(message)
         client.send_message(
                             QueueUrl = queueInfo['QueueUrl'],
-                            MessageBody=clinicInfo
+                            MessageBody=messageJson
                         )
 
     print(result)
