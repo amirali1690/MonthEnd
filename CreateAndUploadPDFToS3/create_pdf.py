914301db6bdb9f2fc7491a0c83391a28aa6e716a
unknown <>
2022-07-11 15:56:26 -0700

    adding new lines to PDF

--- CreateAndUploadPDFToS3/create_pdf.py
+++ CreateAndUploadPDFToS3/create_pdf.py
@@ -9,7 +9,12 @@ def create_pdf(data):
     '''
     pdf=FPDF()
     pdf.add_page()
-    pdf.set_font("Arial",size=15)
-    pdf.cell(200,10,txt=data['clinic'],ln=1,align='C')
+    pdf.set_font("Times",'B',size=15)
+    pdf.set_text_color(100,100,100)
+    pdf.cell(200,30,txt='Ateah Management, LLC',ln=1,align='C')
+    pdf.set_font("Arial",size=12)
+    pdf.set_text_color(0,0,0)
+    pdf.cell(200,6,txt='Clinic: '+data['clinic'],ln=1,align='L')
+    pdf.cell(200,6,txt='Month: '+data['month'],ln=1,align='L')
     pdf.output('/tmp/'+data['month']+'-'+data['clinic']+'.pdf')
     return True
