8add6343351734fcb9d702eb65083c855c553448
unknown <>
2022-07-11 13:17:37 -0700

    changed lambda role permissions

--- CreateAndUploadPDFToS3/lambda_function.py
+++ CreateAndUploadPDFToS3/lambda_function.py
@@ -21,8 +21,7 @@ def lambda_handler(event, context):
     logger.info(event)
     print("Lambda function memory limits in MB:",context.memory_limit_in_mb)
 
-    pdf=FPDF()
-    pdf.add_page()
+    s3_client= boto3.client('s3')
     client = boto3.client('sqs',region_name='us-west-2')
     responses = client.receive_message(
                     QueueUrl = 'https://sqs.us-west-2.amazonaws.com/849779278892/MonthEnd',
@@ -34,5 +33,11 @@ def lambda_handler(event, context):
         for message in responses['Messages']:
             data=ast.literal_eval(message['Body'])
             print(data,data.keys())
+            pdf=FPDF()
+            pdf.add_page()
+            pdf.set_font("Arial",size=15)
+            pdf.cell(200,10,txt=data['clinic'],ln=1,align='C')
+            pdf.output('/tmp/'+data['clinic']+'.pdf')
+            s3_client.upload_file('/tmp/'+data['clinic']+'.pdf','monthly-report-bfhInfo',data['clinic']+'.pdf')
         counter+=1
     return True
