385375a944fd43f7d6c46ca1e08eb1a86b3d67bf
unknown <>
2022-07-12 09:18:01 -0700

    removed extra $ sign in strings

--- CreateAndUploadPDFToS3/create_pdf.py
+++ CreateAndUploadPDFToS3/create_pdf.py
@@ -20,22 +20,22 @@ def create_pdf(data):
     pdf.set_text_color(0,0,0)
     pdf.cell(200,10,txt='Clinic: '+data['clinic'],ln=1,align='L')
     pdf.cell(200,10,txt='Month: '+data['month'].replace('-',' '),ln=1,align='L')
-    pdf.cell(200,10,txt='Current Billed Amount: $'+
+    pdf.cell(200,10,txt='Current Billed Amount: '+
             locale.currency(float(data['billed']['total']['current']),grouping=True),ln=1,align='L')
-    pdf.cell(200,10,txt='Insurance: $'+
+    pdf.cell(200,10,txt='Insurance: '+
             locale.currency(float(data['billed']['insurance']['current']),grouping=True),
             ln=1,align='L')
-    pdf.cell(200,10,txt='PI: $'+
+    pdf.cell(200,10,txt='PI: '+
             locale.currency(float(data['billed']['pi']['current']),grouping=True),ln=1,align='L')
     pdf.cell(200,12,txt='',ln=1,align='L')
 
-    pdf.cell(200,10,txt='Previous Month Billed Amount: $'+
+    pdf.cell(200,10,txt='Previous Month Billed Amount: '+
             locale.currency(float(data['billed']['total']['previous']),grouping=True),
             ln=1,align='L')
-    pdf.cell(200,10,txt='Insurance: $'+
+    pdf.cell(200,10,txt='Insurance: '+
             locale.currency(float(data['billed']['insurance']['previous']),grouping=True),
             ln=1,align='L')
-    pdf.cell(200,10,txt='PI: $'+
+    pdf.cell(200,10,txt='PI: '+
             locale.currency(float(data['billed']['pi']['previous']),grouping=True),ln=1,align='L')
     insuranceCollection= str(
                             round(
@@ -58,24 +58,24 @@ def create_pdf(data):
                                 ,2)
                             )
 
-    pdf.cell(200,10,txt='Insurance Collections: $'+
+    pdf.cell(200,10,txt='Insurance Collections: '+
                     locale.currency(float(data['collection']['insurance']
                     ),grouping=True)+
                     ' + ('+locale.currency(float(data['collection']['copay/coins/ded']
-                    ),grouping=True)+') = $'+
+                    ),grouping=True)+') = '+
                     locale.currency(float(insuranceCollection),grouping=True),ln=1,align='L')
-    pdf.cell(200,10,txt='PI: $'+
+    pdf.cell(200,10,txt='PI: '+
                         locale.currency(float(data['collection']['pi']),grouping=True),
                         ln=1,align='L')
-    pdf.cell(200,10,txt='Copays/Coins/Ded: $'+
+    pdf.cell(200,10,txt='Copays/Coins/Ded: '+
                     locale.currency(float(data['collection']['copay/coins/ded']),grouping=True),
             ln=1,align='L')
-    pdf.cell(200,10,txt='OTC Collecetions: $'+
+    pdf.cell(200,10,txt='OTC Collecetions: '+
                     locale.currency(float(data['collection']['otc']),grouping=True)+
                     ' - ('+locale.currency(float(data['collection']['copay/coins/ded']),
-                    grouping=True)+') = $'+
+                    grouping=True)+') = '+
                     locale.currency(float(otcCollections),grouping=True),ln=1,align='L')
-    pdf.cell(200,10,txt='Total Collections: $'+
+    pdf.cell(200,10,txt='Total Collections: '+
             locale.currency(float(totalCollection),grouping=True),ln=1,align='L')
 
 
@@ -88,18 +88,18 @@ def create_pdf(data):
                                 )
     pdf.cell(200,12,txt='',ln=1,align='L')
     pdf.cell(200,10,txt='Percentage of Collections: '+
-                locale.currency(float(collectionPercentage),grouping=True)+'%',ln=1,align='L')
+                float(collectionPercentage)+'%',ln=1,align='L')
     pdf.cell(200,12,txt='',ln=1,align='L')
-    pdf.cell(200,10,txt='Chiropractic charges: $'+
+    pdf.cell(200,10,txt='Chiropractic charges: '+
             locale.currency(float(data['specialty']['chiro']),grouping=True),
             ln=1,align='L')
-    pdf.cell(200,10,txt='Medical charges: $'+
+    pdf.cell(200,10,txt='Medical charges: '+
                 locale.currency(float(data['specialty']['md']),grouping=True),ln=1,align='L')
     try:
-        pdf.cell(200,10,txt='DPT charges: $'+
+        pdf.cell(200,10,txt='DPT charges: '+
             locale.currency(float(data['specialty']['pt']),grouping=True),ln=1,align='L')
     except ValueError:
-        pdf.cell(200,10,txt='DPT charges: $'+
+        pdf.cell(200,10,txt='DPT charges: '+
             locale.currency(float(0),grouping=True),ln=1,align='L')
     pdf.output('/tmp/'+data['month']+'-'+data['clinic']+'.pdf')
     return True
