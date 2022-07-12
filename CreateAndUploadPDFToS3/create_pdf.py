4c590f23e9d0e7cdcb475d749ebdaa2ffb68238e
unknown <>
2022-07-12 08:42:38 -0700

    change the pdf format

--- CreateAndUploadPDFToS3/create_pdf.py
+++ CreateAndUploadPDFToS3/create_pdf.py
@@ -9,53 +9,65 @@ def create_pdf(data):
     '''
     pdf=FPDF()
     pdf.add_page()
-    pdf.set_font("Times",'B',size=15)
+    pdf.set_font("Arial",'B',size=15)
     pdf.set_text_color(100,100,100)
-    pdf.cell(200,30,txt='Ateah Management, LLC',ln=1,align='C')
-    pdf.set_font("Arial",size=12)
+    pdf.cell(200,12,txt='Ateah Management, LLC',ln=1,align='C')
+    pdf.cell(200,12,txt='',ln=1,align='L')
+    pdf.set_font("Arial",size=11)
     pdf.set_text_color(0,0,0)
-    pdf.cell(200,6,txt='Clinic: '+data['clinic'],ln=1,align='L')
-    pdf.cell(200,6,txt='Month: '+data['month'].replace('-',' '),ln=1,align='L')
-    pdf.cell(200,6,txt='Current Billed Amount: $'+data['billed']['total']['current'],ln=1,align='L')
-    pdf.cell(200,6,txt='Insurance: $'+data['billed']['insurance']['current'],
+    pdf.cell(200,10,txt='Clinic: '+data['clinic'],ln=1,align='L')
+    pdf.cell(200,10,txt='Month: '+data['month'].replace('-',' '),ln=1,align='L')
+    pdf.cell(200,10,txt='Current Billed Amount: $'+
+            "{:,}".format(float(data['billed']['total']['current'])),ln=1,align='L')
+    pdf.cell(200,10,txt='Insurance: $'+"{:,}".format(float(data['billed']['insurance']['current'])),
             ln=1,align='L')
-    pdf.cell(200,6,txt='PI: $'+data['billed']['PI']['current'],ln=1,align='L')
+    pdf.cell(200,10,txt='PI: $'+
+            "{:,}".format(float(data['billed']['pi']['current'])),ln=1,align='L')
+    pdf.cell(200,12,txt='',ln=1,align='L')
 
-    pdf.cell(200,30,txt='Previous Month Billed Amount: $'+data['billed']['total']['previous'],
+    pdf.cell(200,10,txt='Previous Month Billed Amount: $'+
+            "{:,}".format(float(data['billed']['total']['previous'])),
             ln=1,align='L')
-    pdf.cell(200,6,txt='Insurance: $'+data['billed']['insurance']['previous'],
+    pdf.cell(200,10,txt='Insurance: $'+
+            "{:,}".format(float(data['billed']['insurance']['previous'])),
             ln=1,align='L')
-    pdf.cell(200,6,txt='PI: $'+data['billed']['PI']['previous'],ln=1,align='L')
+    pdf.cell(200,10,txt='PI: $'+
+            "{:,}".format(float(data['billed']['pi']['previous'])),ln=1,align='L')
     insuranceCollection= str(
                             round(
-                                float(data['billed']['collection']['insurance'])+
-                                float(data['billed']['collection']['copay/coins/ded'])
+                                float(data['collection']['insurance'])+
+                                float(data['collection']['copay/coins/ded'])
                                 ,2)
                             )
+    pdf.cell(200,12,txt='',ln=1,align='L')
     otcCollections = str(
                             round(
-                                float(data['billed']['collection']['otc'])-
-                                float(data['billed']['collection']['copay/coins/ded'])
+                                float(data['collection']['otc'])-
+                                float(data['collection']['copay/coins/ded'])
                                 ,2)
                             )
     totalCollection = str(
                             round(
-                                float(data['billed']['collection']['insurance'])+
-                                float(data['billed']['collection']['pi'])+
-                                float(data['billed']['collection']['otc'])
+                                float(data['collection']['insurance'])+
+                                float(data['collection']['pi'])+
+                                float(data['collection']['otc'])
                                 ,2)
                             )
 
-    pdf.cell(200,30,txt='insurance Collections: $'+data['billed']['collection']['insurance']+
-                    ' + ( '+data['billed']['collection']['copay/coins/ded']+') = $'+
-                    insuranceCollection,ln=1,align='L')
-    pdf.cell(200,6,txt='PI: $'+data['billed']['collection']['pi'],ln=1,align='L')
-    pdf.cell(200,6,txt='Copays/Coins/Ded: $'+data['billed']['collection']['copay/coins/ded'],
+    pdf.cell(200,10,txt='Insurance Collections: $'+
+                    "{:,}".format(float(data['collection']['insurance']))+
+                    ' + ('+"{:,}".format(float(data['collection']['copay/coins/ded']))+') = $'+
+                    "{:,}".format(float(insuranceCollection)),ln=1,align='L')
+    pdf.cell(200,10,txt='PI: $'+
+                        "{:,}".format(float(data['collection']['pi'])),ln=1,align='L')
+    pdf.cell(200,10,txt='Copays/Coins/Ded: $'+
+                    "{:,}".format(float(data['collection']['copay/coins/ded'])),
             ln=1,align='L')
-    pdf.cell(200,6,txt='otc: $'+data['billed']['collection']['otc']+
-                    ' - ( '+data['billed']['collection']['copay/coins/ded']+') = $'+
-                    otcCollections,ln=1,align='L')
-    pdf.cell(200,6,txt='Total Collections: $'+totalCollection,ln=1,align='L')
+    pdf.cell(200,10,txt='OTC Collecetions: $'+
+                    "{:,}".format(float(data['collection']['otc']))+
+                    ' - ('+"{:,}".format(float(data['collection']['copay/coins/ded']))+') = $'+
+                    "{:,}".format(float(otcCollections)),ln=1,align='L')
+    pdf.cell(200,10,txt='Total Collections: $'+"{:,}".format(float(totalCollection)),ln=1,align='L')
 
 
     collectionPercentage = str(
@@ -65,12 +77,20 @@ def create_pdf(data):
                                     ,2
                                     )*100
                                 )
-    pdf.cell(200,30,txt='Percentage of Collections: %'+collectionPercentage,ln=1,align='L')
-
-    pdf.cell(200,30,txt='Chiropractic charges: $'+data['billed']['specialty']['chiro'],
+    pdf.cell(200,12,txt='',ln=1,align='L')
+    pdf.cell(200,10,txt='Percentage of Collections: '+
+                "{:,}".format(float(collectionPercentage))+'%',ln=1,align='L')
+    pdf.cell(200,12,txt='',ln=1,align='L')
+    pdf.cell(200,10,txt='Chiropractic charges: $'+
+            "{:,}".format(float(data['specialty']['chiro'])),
             ln=1,align='L')
-    pdf.cell(200,6,txt='Medical charges: $'+data['billed']['specialty']['md'],ln=1,align='L')
-    pdf.cell(200,6,txt='DPT charges: $'+data['billed']['specialty']['pt'],ln=1,align='L')
-
+    pdf.cell(200,10,txt='Medical charges: $'+
+                "{:,}".format(float(data['specialty']['md'])),ln=1,align='L')
+    try:
+        pdf.cell(200,10,txt='DPT charges: $'+
+            "{:,}".format(float(data['specialty']['pt'])),ln=1,align='L')
+    except ValueError:
+        pdf.cell(200,10,txt='DPT charges: $'+
+            "{:,}".format(0),ln=1,align='L')
     pdf.output('/tmp/'+data['month']+'-'+data['clinic']+'.pdf')
     return True
